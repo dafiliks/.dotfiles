4801aa2c9f21b4d32a6d03a9e6eb305d2eb1b2ed
David Filiks <davidfiliks55@gmail.com>
2025-04-26 15:53:58 +0100

    Changes

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,8 +1,8 @@
-function ColorMyGruber(_, opts)
+function ColorMyRosePine(_, opts)
     vim.opt.background = "dark"
     vim.opt.termguicolors = true
-    require("gruber-darker").setup(opts)
-    vim.cmd.colorscheme("gruber-darker")
+    require("rose-pine").setup(opts)
+    vim.cmd.colorscheme("rose-pine")
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -10,7 +10,7 @@ end
 
 return {
     "chrisbra/Colorizer",
-    { "blazkowolf/gruber-darker.nvim", opts = { bold = false, italic = {
-      strings = false, comments = false } },
-    name = "gruber-darker", config = ColorMyGruber },
+    { "rose-pine/neovim", opts = { bold = false, italic = {
+        strings = false, comments = false } },
+    name = "rose-pine", config = ColorMyRosePine },
 }
